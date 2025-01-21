165e6f9edea2c1c3c1e32026c8cff918f35d48c5
Eren <155644544+ErenYeager08@users.noreply.github.com>
2025-01-21 22:18:52 +0330

    Add files via upload

---
+++ FashionHub/README.md
@@ -0,0 +1,4 @@
+# FashionHub v1.0
+# https://github.com/ErenYeager08/FashionHub
+# UI created by https://chatgpt.com
+# image and logo created by DALL-E
---
+++ FashionHub/include/s.php
@@ -0,0 +1,10 @@
+<style>
+        .nav-link{
+            font-size: 1.2rem;    
+            font-weight: bold;
+        }
+        .navbar-brand {
+            font-weight: bold;
+            font-size: 1.8rem;
+        }
+    </style>
